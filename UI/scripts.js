9788d961a262f474e870c77a0c06f9fe9e491779
AKazak <anton.kazak.26.1997@gmail.com>
2016-04-02 17:21:30 +0300

    [lab 5] add localstorage

--- UI/scripts.js
+++ UI/scripts.js
@@ -1,4 +1,5 @@
 var author = "Guest";
+var messageList = [];
 var textBox = document.getElementById('textBox');
 
 function run(){
@@ -10,6 +11,9 @@ function run(){
 
     var appContainer = document.getElementsByClassName('textBox')[0];
     appContainer.addEventListener('click', delegateEvent);
+
+    loadFromStorage();
+    render();
 }
 
 function getText(el) {
@@ -19,6 +23,133 @@ function getText(el) {
     return pre.innerHTML;
 }
 
+function newMessage(text, author, edit, time, timestamp, del, me) {
+    return {
+            message: text,
+            auth: author,
+            isEdit: !!edit,
+            timestamp: time,
+            editTimestamp: timestamp,
+            id: uniqueId(),
+            isDeleted: del,
+            isMine: !!me
+    };
+}
+
+function uniqueId() {
+    var date = Date.now();
+    var random = Math.random() * Math.random();
+    return Math.floor(date * random);
+}
+
+function render(){
+    for(var i = 0; i < messageList.length; i++){
+        renderMessage(messageList[i]);
+    }
+    renderAuthor(author);
+}
+
+function renderMessage(mess) {
+    var list = document.getElementsByClassName('textBox')[0];
+    if (mess.isMine) {
+        var element = myElementFromTemplate();
+        renderMyMessageAttributes(element, mess);
+    }
+    else {
+        var element = elementFromTemplate();
+        renderMessageAttributes(element, mess);
+    }
+    list.appendChild(element);
+    element.scrollIntoView();
+}
+
+function renderAuthor(author){
+    document.getElementById("me").innerHTML = author;
+}
+
+function myElementFromTemplate() {
+    var template = document.getElementById("my-message-template");
+    return template.firstElementChild.cloneNode(true);
+}
+
+function elementFromTemplate() {
+    var template = document.getElementById("others-message-template");
+    return template.firstElementChild.cloneNode(true);
+}
+
+function renderMessageAttributes(element, mess) {
+    element.setAttribute('data-message-id', mess.id);
+    element.lastElementChild.textContent = mess.message;
+    if (mess.isEdit) {
+        element.firstElementChild.textContent = mess.auth + ", " + mess.timestamp;
+        element.firstElementChild.nextElementSibling.textContent = "Edited " + mess.editTimestamp;
+    }
+    else element.firstElementChild.textContent = mess.auth + ", " + mess.timestamp;
+    if (mess.isDeleted) {
+        element.lastElementChild.style.opacity = "0.5";
+        element.lastElementChild.textContent = "This message has been deleted.";
+    }
+}
+
+function renderMyMessageAttributes(element, mess) {
+    var iconEdit = '<a href="#"><i class="fa fa-pencil fa-fw edit"></i></a>';
+    var iconDelete = '<a href="#"><i class="fa fa-trash-o fa-lg delete"></i></a>'
+    element.setAttribute('data-message-id', mess.id);
+    element.lastElementChild.textContent = mess.message;
+    if (mess.isEdit) {
+        element.firstElementChild.innerHTML = mess.auth + ", " + mess.timestamp + iconEdit + iconDelete;
+        element.firstElementChild.nextElementSibling.textContent = "Edited " + mess.editTimestamp;
+    }
+    else {
+        element.firstElementChild.innerHTML = mess.auth + ", " + mess.timestamp + iconEdit + iconDelete;
+    }
+    if (mess.isDeleted) {
+        element.firstElementChild.firstElementChild.nextElementSibling.remove();
+        element.firstElementChild.firstElementChild.remove();
+        element.lastElementChild.style.opacity = "0.5";
+        element.lastElementChild.textContent = "This message has been deleted.";
+    }
+}
+
+function saveMessages(listToSave) {
+    if (typeof(Storage) == "undefined") {
+        swal('localStorage is not accessible');
+        return;
+    }
+    localStorage.setItem("Message history", JSON.stringify(listToSave));
+}
+
+function loadMessages() {
+    if (typeof(Storage) == "undefined") {
+        swal('localStorage is not accessible');
+        return;
+    }
+    var item = localStorage.getItem("Message history");
+    return item && JSON.parse(item);
+}
+
+function loadFromStorage(){
+    author = loadAuthor() || "Guest";
+    messageList = loadMessages() || [newMessage("Welcome to our chart!", "System", false, (new Date()).toLocaleString(), false, false)];
+}
+
+function saveAuthor(){
+    if (typeof(Storage) == "undefined") {
+        swal('localStorage is not accessible');
+        return;
+    }
+    localStorage.setItem("Author", author);
+}
+
+function loadAuthor(){
+    if (typeof(Storage) == "undefined") {
+        swal('localStorage is not accessible');
+        return;
+    }
+    var item = localStorage.getItem("Author");
+    return item;
+}
+
 function send(e) {
     e.preventDefault();
     var messageInput = document.getElementById('message');
@@ -26,17 +157,12 @@ function send(e) {
         return false;
     } else {
         var text = getText(messageInput.value);
-        var message = '<div class="myBubble">' + text + '</div>';
         var date = new Date();
-        var authorInfo = '<div class="myInfo">' + date.toLocaleString() + ' ' + author + ' ' +
-            '<a href="#"><i class="fa fa-pencil fa-fw edit"></i></a>' +
-            '<a href="#"><i class="fa fa-trash-o fa-lg delete"></i></a>' + '</div>';
         messageInput.value = "";
-        var messageBox = document.createElement('div');
-        messageBox.classList.add("messageBlock");
-        messageBox.innerHTML = authorInfo + message;
-        textBox.appendChild(messageBox);
-        messageBox.scrollIntoView();
+        var message = newMessage(text, author, false, date.toLocaleString(), 0, false, true);
+        renderMessage(message);
+        messageList.push(message);
+        saveMessages(messageList);
     }
 }
 
@@ -53,6 +179,7 @@ function login(e) {
     loginBox.style.display = "none";
     var logInfo = document.getElementById('logInfo');
     logInfo.style.display = "inline";
+    saveAuthor(author);
 }
 
 function editName() {
@@ -63,6 +190,7 @@ function editName() {
         showCancelButton: true,
         closeOnConfirm: false,
         animation: "pop",
+        inputValue: author,
         inputPlaceholder: "Write something"
     }, function (inputValue) {
         if (inputValue === false) return false;
@@ -79,13 +207,12 @@ function editName() {
         user.innerHTML = author;
         var me = document.getElementById('me');
         me.innerHTML = author;
+        saveAuthor(author);
         swal("Nice!", "Your new name: " + inputValue, "success");
     });
 }
 
 function delegateEvent(evtObj) {
-    console.log("asd");
-    console.log(evtObj.target);
     if(evtObj.type === 'click' && evtObj.target.classList.contains('delete')){
         var messageBlock = evtObj.target.parentNode.parentNode.parentNode;
         deleteMessage(messageBlock);
@@ -111,10 +238,17 @@ function deleteMessage(messageBlock) {
         closeOnCancel: false
     }, function (isConfirm) {
         if (isConfirm) {
-            iconEdit.remove();
             iconDelete.remove();
+            iconEdit.remove();
+            var messageId = messageBlock.getAttribute("data-message-id");
+            for (var i = 0; i < messageList.length; i++) {
+                if (messageList[i].id == messageId) {
+                    messageList[i].isDeleted = true;
+                }
+            }
             message.innerHTML = "This message has been deleted.";
             message.classList.add("deleted");
+            saveMessages(messageList);
             swal("Deleted!", "Your message has been deleted.", "success");
         } else {
             swal("Cancelled", "Your message is safe :)", "error");
@@ -132,6 +266,7 @@ function editMessage(messageBlock) {
         showCancelButton: true,
         closeOnConfirm: false,
         animation: "pop",
+        inputValue: messageText,
         inputPlaceholder: "Enter your message..."
     }, function (inputValue) {
         if (inputValue === false) return false;
@@ -143,12 +278,20 @@ function editMessage(messageBlock) {
             swal.close();
             return false;
         }
+        var messageId = messageBlock.getAttribute("data-message-id");
+        for (var i = 0; i < messageList.length; i++) {
+            if (messageList[i].id == messageId) {
+                messageList[i].isEdit = true;
+                messageList[i].message = getText(inputValue);
+                messageList[i].editTimestamp = new Date().toLocaleString();
+            }
+        }
         message.innerHTML = getText(inputValue);
         var myInfo = messageBlock.firstElementChild;
-        var info = document.createElement('div');
+        var info = myInfo.nextElementSibling;
         var editInfo = "Edited  " + new Date().toLocaleString();
         info.innerHTML = editInfo;
-        myInfo.appendChild(info);
+        saveMessages(messageList);
         swal.close();
     });
 }
